143687a69999555caa279e0ac3e3ea77b9b8290e
Guillaume DUALE <guillaume.duale@virtual-expo.com>
2019-04-03 14:02:22 +0200

    Setup disk OK.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,39 @@
-mkdir /mnt/exherbo && mount /dev/sda2 /mnt/exherbo
+#!/bin/bash
+##Setup Exherbo
+
+dd='/dev/sda'
+#Eraze disk
+dd if=/dev/zero of=$dd bs=512 count=1
+
+#Make partitions
+echo -e "n\n\n\n\n+500M\nw\n" | fdisk $dd
+echo -e "n\n\n\n\n+5G\nw\n" | fdisk $dd
+echo -e "n\n\n\n\n\nw\n" | fdisk $dd
+
+mkfs.ext2 /dev/sda1
+mkfs.ext4 /dev/sda2
+mkfs.ext4 /dev/sda3
+
+mkdir /mnt/exherbo && mount /dev/sda2 /mnt/exherbo && cd /mnt/exherbo
+curl -O http://dev.exherbo.org/stages/exherbo-x86_64-pc-linux-gnu-current.tar.xz
+curl -O http://dev.exherbo.org/stages/sha1sum
+grep exherbo-x86_64-pc-linux-gnu-current.tar.xz sha1sum | sha1sum -c
+if [ $? -ne 0 ];then
+  echo "sha1sum ERROR"
+  exit 1
+fi
+
+tar xJpf exherbo*xz
+
+echo "/dev/sda2    /               ext4      defaults    0 0" > etc/fstab
+echo "/dev/sda3    /home           ext4      defaults    0 2" >> etc/fstab
+echo "/dev/sda1    /boot           ext2      defaults    0 0" >> etc/fstab
+
+
+exit 1
+#Chroot into the system
+
+
 mount -o bind /sys /mnt/exherbo/sys/
 mount -t proc none /mnt/exherbo/proc/
 mount /dev/sda1 /mnt/exherbo/boot/
