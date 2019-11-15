90e89be9fa3987b7b12de0401d0ccd89b7c93f28
Guillaume DUALE <guillaume.duale@virtual-expo.com>
2019-11-15 21:51:32 +0100

    test

--- setup.sh
+++ setup.sh
@@ -6,13 +6,13 @@ dd='/dev/sda'
 dd if=/dev/zero of=$dd bs=512 count=1
 
 #Make partitions
-echo -e "n\n\n\n\n+500M\nw\n" | fdisk $dd
-echo -e "n\n\n\n\n+5G\nw\n" | fdisk $dd
-echo -e "n\n\n\n\n\nw\n" | fdisk $dd
+echo -e "n\n\n\n\n+500M\nw\n" | fdisk $dd #/boot
+echo -e "n\n\n\n\n+5G\nw\n" | fdisk $dd #/
+echo -e "n\n\n\n\n\nw\n" | fdisk $dd #/home
 
-mkfs.ext2 /dev/sda1
-mkfs.ext4 /dev/sda2
-mkfs.ext4 /dev/sda3
+mkfs.ext2 /dev/sda1 #/boot 
+mkfs.ext4 /dev/sda2 #/
+mkfs.ext4 /dev/sda3 #/home
 
 mkdir /mnt/exherbo && mount /dev/sda2 /mnt/exherbo && cd /mnt/exherbo
 curl -O http://dev.exherbo.org/stages/exherbo-x86_64-pc-linux-gnu-current.tar.xz
@@ -34,10 +34,14 @@ exit 1
 #Chroot into the system
 
 
+mount -o rbind /dev /mnt/exherbo/dev/
 mount -o bind /sys /mnt/exherbo/sys/
 mount -t proc none /mnt/exherbo/proc/
 mount /dev/sda1 /mnt/exherbo/boot/
 mount /dev/sda3 /mnt/exherbo/home/
 cp /etc/resolv.conf /mnt/exherbo/etc/resolv.conf
 env -i TERM=$TERM SHELL=/bin/bash HOME=$HOME $(which chroot) /mnt/exherbo /bin/bash
+source /etc/profile
+export PS1="(chroot) $PS1"
 
+echo "étape : cave sync...(todo)"
