11812365243be1952f283794b575490754c6aa4f
Guillaume DUALE <guillaume.duale@virtual-expo.com>
2019-11-21 10:45:02 +0100

    resolve conflict

--- step2_chroot.sh
+++ step2_chroot.sh
@@ -8,8 +8,48 @@ echo "étape : cave sync...(todo)"
 cave sync
 
 #Kernel
-wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.3.11.tar.xz
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.3.12.tar.xz
 tar xJf linux-5*
-cd linux-5.3.11
-pwd
+cd linux-5.3.12
+make nconfig
+make
+make modules_install
+cp arch/x86/boot/bzImage /boot/kernel
 
+#Systemd
+echo "*/* systemd" >> /etc/paludis/options.conf
+cave resolve -x sys-apps/Systemd
+cave resolve world -cx
+eclectic init list
+echo "exherbo" > /etc/hostname
+cave sync
+cave resolve world -c
+cave resolve --execute --preserve-world --skip-phase test sys-apps/systemd
+
+#Grub
+grub-install /dev/sda
+cat<<EOF > /boot/grub/grub.cfg
+set timeout=10
+set default=0
+menuentry "Exherbo" {
+    set root=(hd0,1)
+    linux /kernel root=/dev/sda2
+}
+EOF
+
+#Hostname
+cat<<EOF > /etc/hosts
+127.0.0.1    my-hostname.domain.foo    my-hostname    localhost
+::1          localhost
+EOF
+
+#Password
+echo "Set the root password:"
+passwd
+
+#Locale
+localedef -i fr_FR -f UTF-8 fr_FR
+echo LANG="fr_FR.UTF-8" > /etc/env.d/99locale
+ln -s /usr/share/zoneinfo/Europe/Paris /etc/localtime
+ 
+echo "All is done! You can exit chroot and reboot"
